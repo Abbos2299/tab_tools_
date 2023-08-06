225d4a97c4f6c0aa0867f90240f274368bfd75bd
Abbos <abboskhon.kh@gmail.com>
2023-08-06 18:21:48 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,81 +1,13 @@
-#part 1
-import firebase_admin
-from firebase_admin import credentials
-
-#part 2
-import gdown
-import requests
-import base64
-import os
-import sys
-
-
-
-#part 1
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate('tab-tools-firebase-adminsdk-8ncav-4f5ccee9af.json')
-firebase_admin.initialize_app(cred)
-
-# Now you're connected to Firebase and can use its services
-
-
-
-
-#part 2
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/process', methods=['GET'])
-def handle_request():
-    # Execute your desired functionality here
-    # For example, you can process data, retrieve information from a database, or perform any other task
+@app.route('/launch', methods=['GET'])
+def launch_python_file():
+    # Perform any actions you want to execute when the request is received
+    # For example, you can launch a Python file or execute any other desired functionality
+    print('Request accepted')
     return 'Success'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8501)
-
-
-
-
-def download_file_from_google_drive(file_id, destination):
-    url = f"https://drive.google.com/uc?id={file_id}"
-    gdown.download(url, destination, quiet=False)
-
-# Define your file ID and destination path
-file_id = '1lZB6gg18DayNB63zJd4YcLE4UKtznyL6'
-destination_path = '123.pdf'
-
-# Download the file
-download_file_from_google_drive(file_id, destination_path)
-
-
-def ocr_space_file(filename, language, overlay, ocr_engine):
-    payload = {
-        'apikey': 'K89929856188957',
-        'language': language,
-        'isOverlayRequired': overlay,
-        'OCREngine': ocr_engine,
-    }
-
-    with open(filename, 'rb') as f:
-        result = requests.post('https://api.ocr.space/parse/image', 
-                               files={filename: f},
-                               data=payload).json()
-
-    if 'ParsedResults' in result:
-        for parsed_result in result['ParsedResults']:
-            print(parsed_result['ParsedText'])
-    else:
-        print("Error occurred: ", result['ErrorMessage'])
-
-if __name__ == "__main__":
-    filename = '123.pdf'
-    language = 'eng'
-    overlay = False
-    ocr_engine = 2
-
-    ocr_space_file(filename, language, overlay, ocr_engine)
-
-
-
