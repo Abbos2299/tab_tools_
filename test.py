23d722c33d504ac50ea4297da134dc4951edb13e
Abbos <abboskhon.kh@gmail.com>
2023-08-07 14:14:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,36 @@
 from flask import Flask, request
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+from datetime import timedelta
 
 app = Flask(__name__)
+cred = credentials.Certificate('tab-tools-firebase-adminsdk-8ncav-4f5ccee9af.json')
+firebase_admin.initialize_app(cred)
 
 @app.route('/launch', methods=['GET'])
 def launch_python_file():
-
-    
     user_uid = request.args.get('uid')
     print('User UID:', user_uid)
+
+    bucket_name = 'tab-tools.appspot.com'
+    bucket = storage.bucket(bucket_name)
+    folder_name = 'ZFjks5nrhOXbTsT4qhg1gtREPOw1'  # Replace with the appropriate user UID
+    blobs = bucket.list_blobs(prefix=folder_name)
+
+    # Iterate over the blobs and get the last added file
+    last_added_blob = None
+    for blob in blobs:
+        if not last_added_blob or blob.updated > last_added_blob.updated:
+            last_added_blob = blob
+
+    if last_added_blob:
+        file_url = last_added_blob.generate_signed_url(expiration=timedelta(minutes=15))
+        print('Last added file URL:', file_url)
+    else:
+        print('No files found in the folder')
+
     return 'Success'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
