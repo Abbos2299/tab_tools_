820da29d5e74638e9d73f2c306c6bce62cde35b2
Abbos <abboskhon.kh@gmail.com>
2023-08-13 23:43:43 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,21 +1,27 @@
-import requests
+from flask import Flask, request
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from datetime import timedelta
+import requests
 import time
+import urllib.parse
+import os
 
-# Path to your Firebase Admin SDK credentials JSON file
+app = Flask(__name__)
 cred = credentials.Certificate('tab-tools-firebase-adminsdk-8ncav-4f5ccee9af.json')
-
-# Initialize Firebase app
 firebase_admin.initialize_app(cred)
 
-# Get Firestore client
-db = firestore.client()
+@app.route('/launch', methods=['GET'])
+def launch_python_file():
+    user_uid = request.args.get('uid')
+    print('User UID:', user_uid)
 
-def get_user_info(uid):
+    # Get Firestore client
+    db = firestore.client()
+
     # Access the 'users' collection and retrieve the document with the given UID
-    user_doc_ref = db.collection('users').document(uid)
+    user_doc_ref = db.collection('users').document(user_uid)
     user_doc = user_doc_ref.get()
 
     if user_doc.exists:
@@ -29,44 +35,50 @@ def get_user_info(uid):
             usdot = user_info_doc.get('USDOT')
 
             if mc:
-                return mc, 'MC'
+                mc_usdot = mc
+                search_field = 'MC'
             elif usdot:
-                return usdot, 'USDOT'
+                mc_usdot = usdot
+                search_field = 'USDOT'
             else:
-                return None, None
+                mc_usdot = None
+                search_field = None
+
+            if mc_usdot and search_field:
+                # URL encode the MC/USDOT number
+                mc_usdot_encoded = urllib.parse.quote(mc_usdot)
+
+                # Build the URL for the search
+                url = f'https://safer.fmcsa.dot.gov/CompanySnapshot.aspx?SearchType=Company&Dbn={mc_usdot_encoded}'
+
+                # Send the request to the website
+                response = requests.get(url)
+                time.sleep(3)  # Wait for 3 seconds to allow the page to load
+
+                if 'No records matching' in response.text or 'Record Not Found' in response.text:
+                    # Update Firestore with 'Snapshot check' field
+                    user_doc_ref.update({'Snapshot check': 'No records'})
+                else:
+                    # Parse the response to extract the company phone number
+                    # You may need to use a HTML parsing library like BeautifulSoup for this step
+                    # Assuming the extracted phone number is stored in a variable called 'company_phone'
+                    company_phone = '1234567890'  # Replace with the actual extracted phone number
+
+                    # Update Firestore with 'Company Phone' field
+                    user_doc_ref.update({'Company Phone': company_phone})
+
+                    print(f'Company Phone: {company_phone}')
+
+            else:
+                print('MC or USDOT field is missing in User Info')
+
         else:
-            return None, None
+            print('User Info document not found')
+
     else:
-        return None, None
+        print('User document not found')
 
-def search_company(mc_usdot):
-    url = 'https://safer.fmcsa.dot.gov/CompanySnapshot.aspx'
-    params = {'SearchType': 'Company',
-              'Dbn': mc_usdot}
+    return 'Success'
 
-    response = requests.get(url, params=params)
-    time.sleep(3)  # Wait for 3 seconds to allow the page to load
-
-    if 'No records matching' in response.text or 'Record Not Found' in response.text:
-        return 'No records'
-    else:
-        # Parse the response to extract the company phone number
-        # You may need to use a HTML parsing library like BeautifulSoup for this step
-
-        # Assuming the extracted phone number is stored in a variable called 'company_phone'
-        return company_phone
-
-def update_firestore(uid, mc_usdot, phone):
-    user_doc_ref = db.collection('users').document(uid)
-    user_doc_ref.update({'Snapshot check': 'No records', 'Company Phone': phone})
-
-# Example usage
-user_uid = 'your_user_uid'
-
-mc_usdot, field = get_user_info(user_uid)
-if mc_usdot and field:
-    company_phone = search_company(mc_usdot)
-    if company_phone == 'No records':
-        update_firestore(user_uid, mc_usdot, None)
-    else:
-        update_firestore(user_uid, mc_usdot, company_phone)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
