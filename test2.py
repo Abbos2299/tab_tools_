84c92aadd2e336520bd25ae0b0f18b4fe50208d8
Abbos <abboskhon.kh@gmail.com>
2023-08-14 10:03:25 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -18,8 +18,8 @@ def launch_python_file():
 
     # Get Firestore client
     db = firestore.client()
-            users_ref = db.collection('users')
-            user_doc_ref = users_ref.document(user_uid)
+        users_ref = db.collection('users')
+        user_doc_ref = users_ref.document(user_uid)
 
     print('User document exists:', user_doc.exists)
 
